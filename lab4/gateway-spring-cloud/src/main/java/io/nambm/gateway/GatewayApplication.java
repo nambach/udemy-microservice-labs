433b0176c5992e43b003b9f3c6493c12ac180ceb
nambach <nambach2@gmail.com>
2019-08-20 15:22:47 +0700

    Add API Gateway in Zuul and Spring Cloud

---
+++ lab4/gateway-spring-cloud/src/main/java/io/nambm/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package io.nambm.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
+}
